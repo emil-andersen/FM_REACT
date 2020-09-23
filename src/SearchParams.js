6e7751650be8411de827a777e106804abd65044a
emil-andersen <emil.skouhus@gmail.com>
2020-09-23 04:34:35 +0200

    commit er bare for en sikkerhedsskyld, så jeg ikke behøver at se videoerne om det her igen

---
+++ src/SearchParams.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(event) => setLocation(event.target.value)}
+            onBlur={(event) => setLocation(event.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(event) => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
--- src/app.js
+++ src/app.js
@@ -1,41 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-    /*return React.createElement(
-        "div", {},
-        [
-            React.createElement("h1", {}, "adopt me!"),
-            React.createElement(Pet, {
-                navn: "Tessie",
-                dyr: "hund",
-                race: "Jack Russel Terier"
-            }),
-            React.createElement(Pet, {
-                navn: "Wiktoria",
-                dyr: "Menneske",
-                race: "Kæreste"
-            }),
-            React.createElement(Pet, {
-                navn: "Kevin",
-                dyr: "Menneske",
-                race: "Ikke Kæreste"
-            })
-        ]
-    );*/
-
-        return (
-            <div>
-                <h1>adopt me!</h1>
-                <Pet navn="Tessie" dyr="hund" race="Jack Russel Terier" />
-                <Pet navn="Wiktora" dyr="Menneske" race="Menneske" />
-                <Pet navn="Fillip" dyr="Ikke Kæreste" race="Menneske" />
-            </div>
-
-        )
-
+  return (
+    <div>
+      <h1>adopt me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(<App/>,
-    document.getElementById("root"));
+render(<App />, document.getElementById("root"));
