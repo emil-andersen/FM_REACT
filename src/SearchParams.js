97a2a0c06a04c9d269922b3ca4345e67e2774ac4
emil-andersen <emil.skouhus@gmail.com>
2020-09-23 05:55:09 +0200

    det virker stadig, men..

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,6 +4,8 @@ import { ANIMALS } from "@frontendmasters/pet";
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -31,6 +33,23 @@ const SearchParams = () => {
               <option key={animal} value={animal}>{animal}</option>
             ))}
           </select>
+        </label>
+        <label htmlFor="breed">
+            Breed
+            <select
+                id="breed"
+                value={breed}
+                onChange={event => event.target.value}
+                onBlur={event => event.target.value}
+                disabled={breeds.length === 0}
+            >
+                <option>All</option>
+                    {breeds.map(breedString =>  (
+                        <option key={breedString} value={breedString}>
+                            {breedString}
+                        </option>
+                     ))}
+            </select>
         </label>
         <button>Submit</button>
       </form>
